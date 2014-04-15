3f706ae720044a97f433cbe0a32a5cb62eb24097
Edmundo Fuentes <me@edmundofuentes.com>
2014-04-15 18:03:07 -0500

    Enabling Transaction delete

--- src/FuentesWorks/NickelTrackerBundle/Controller/TransactionController.php
+++ src/FuentesWorks/NickelTrackerBundle/Controller/TransactionController.php
@@ -3,6 +3,9 @@
 namespace FuentesWorks\NickelTrackerBundle\Controller;
 
 use FuentesWorks\NickelTrackerBundle\Entity\TransactionLog;
+use FuentesWorks\NickelTrackerBundle\Entity\Account;
+use FuentesWorks\NickelTrackerBundle\Entity\Category;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,13 +25,20 @@ class TransactionController extends NickelTrackerController
 
         if($status)
         {
-            if($status == 'ok')
+            if($status == 'add')
             {
                 $msg = array('type' => 'success',
                     'text' => "<strong>Woot!</strong> Successfully registered the transaction!");
                 return $this->render('FuentesWorksNickelTrackerBundle:Transaction:list.html.twig',
                         array('transactions' => $transactions,
                               'msg' => $msg));
+            } elseif($status == 'delete')
+            {
+                $msg = array('type' => 'success',
+                    'text' => "<strong>Woot!</strong> Successfully deleted the transaction!");
+                return $this->render('FuentesWorksNickelTrackerBundle:Transaction:list.html.twig',
+                    array('transactions' => $transactions,
+                        'msg' => $msg));
             }
         }
 
@@ -56,13 +66,14 @@ class TransactionController extends NickelTrackerController
         $doctrine = $this->getDoctrine();
         $em = $doctrine->getManager();
 
+        /* @var Account $account */
+        $account = $doctrine->getRepository('FuentesWorks\NickelTrackerBundle\Entity\Account')
+            ->find( $request->request->get('accountId') );
+        /* @var Category $category */
+        $category = $doctrine->getRepository('FuentesWorks\NickelTrackerBundle\Entity\Category')
+            ->find( $request->request->get('categoryId') );
+
         $trans = new TransactionLog();
-
-        $accountId = $request->request->get('accountId');
-        $account = $em->getReference('FuentesWorks\NickelTrackerBundle\Entity\Account', $accountId);
-        $categoryId = $request->request->get('categoryId');
-        $category = $em->getReference('FuentesWorks\NickelTrackerBundle\Entity\Category', $categoryId);
-
         $trans->setType('I');
         $trans->setAccountId($account);
         $trans->setCategoryId($category);
@@ -71,22 +82,82 @@ class TransactionController extends NickelTrackerController
         $trans->setAmount($request->request->get('amount'));
         $trans->setDescription($request->request->get('description'));
 
+        $account->updateBalance($trans);
+
         $em->persist($trans);
         $em->flush();
 
-
-
-        return $this->redirect($this->generateUrl('fuentesworks_nickeltracker_transaction_list', array('status' => 'ok')));
+        return $this->redirect($this->generateUrl('fuentesworks_nickeltracker_transaction_list', array('status' => 'add')));
     }
 
     public function newExpenseProcessAction(Request $request)
     {
-        return $this->render('FuentesWorksNickelTrackerBundle:Transaction:list.html.twig');
+        $doctrine = $this->getDoctrine();
+        $em = $doctrine->getManager();
+
+        /* @var Account $account */
+        $account = $doctrine->getRepository('FuentesWorks\NickelTrackerBundle\Entity\Account')
+            ->find( $request->request->get('accountId') );
+        /* @var Category $category */
+        $category = $doctrine->getRepository('FuentesWorks\NickelTrackerBundle\Entity\Category')
+            ->find( $request->request->get('categoryId') );
+
+        $trans = new TransactionLog();
+        $trans->setType('E');
+        $trans->setAccountId($account);
+        $trans->setCategoryId($category);
+
+        $trans->setDate(new \DateTime($request->request->get('date')));
+        $trans->setAmount($request->request->get('amount'));
+        $trans->setDescription($request->request->get('description'));
+
+        $account->updateBalance($trans);
+
+        $em->persist($trans);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('fuentesworks_nickeltracker_transaction_list', array('status' => 'add')));
     }
 
     public function newTransferProcessAction(Request $request)
     {
         return $this->render('FuentesWorksNickelTrackerBundle:Transaction:list.html.twig');
+    }
+
+    public function deleteAction(Request $request)
+    {
+        $doctrine = $this->getDoctrine();
+        $em = $doctrine->getManager();
+        $globalId = $request->request->get('globalId');
+
+        if(!$globalId)
+        {
+            // No Global ID fround
+            throw $this->createNotFoundException('No GlobalId provided');
+        } elseif($globalId[0] == 'T') {
+            // TransferLog
+            $id = substr($globalId, 1);
+            $trans = $doctrine->getRepository('FuentesWorks\NickelTrackerBundle\Entity\TransferLog')
+                ->find( $id );
+        } elseif($globalId[0] == 'I' or $globalId[0] == 'E') {
+            // TransactionLog
+            $id = substr($globalId, 1);
+            $trans = $doctrine->getRepository('FuentesWorks\NickelTrackerBundle\Entity\TransactionLog')
+                ->find( $id );
+        } else {
+            // Invalid GlobalId
+            throw $this->createNotFoundException('Invalid GlobalId provided');
+        }
+
+        if(!$trans)
+        {
+            throw $this->createNotFoundException('Could not find log with ID: ' . $globalId );
+        }
+
+        $em->remove($trans);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('fuentesworks_nickeltracker_transaction_list', array('status' => 'delete')));
     }
 
 }
