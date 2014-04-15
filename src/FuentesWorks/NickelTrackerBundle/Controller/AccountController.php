d59a70de10a8d08fb1fb55bee3a9475dc184c52e
Edmundo Fuentes <me@edmundofuentes.com>
2014-04-14 23:04:36 -0500

    Account save and view

--- src/FuentesWorks/NickelTrackerBundle/Controller/AccountController.php
+++ src/FuentesWorks/NickelTrackerBundle/Controller/AccountController.php
@@ -27,4 +27,41 @@ class AccountController extends NickelTrackerController
                   'mode' => 'new'));
     }
 
+    public function newProcessAction(Request $request)
+    {
+        $doctrine = $this->getDoctrine();
+        $em = $doctrine->getManager();
+
+        $account = new Account();
+        $account->setName($request->request->get('name'));
+        $account->setType($request->request->get('type'));
+
+        $em->persist($account);
+        $em->flush();
+
+        $msg = array('type' => 'success',
+                     'text' => "<strong>Woot!</strong> Account created successfully!");
+
+        return $this->render('FuentesWorksNickelTrackerBundle:Account:list.html.twig',
+            array('msg' => $msg));
+    }
+
+    public function viewAction($id)
+    {
+        $account = $this->getDoctrine()->getRepository('FuentesWorksNickelTrackerBundle:Account')
+            ->find($id);
+
+        if(!$account)
+        {
+            $msg = array('type' => 'warning',
+                    'text' => "<strong>Woah!</strong> Could not load account with id <strong>" . $id . "</strong>!");
+            return $this->render('FuentesWorksNickelTrackerBundle:Account:list.html.twig',
+                array('msg' => $msg));
+        }
+
+        return $this->render('FuentesWorksNickelTrackerBundle:Account:detail.html.twig',
+            array('account' => $account,
+                  'mode' => 'view'));
+    }
+
 }
