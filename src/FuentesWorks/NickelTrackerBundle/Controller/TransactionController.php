ff3d006f73364f08203783a8c533a3c83b42f7ab
Edmundo Fuentes <me@edmundofuentes.com>
2014-06-12 20:34:52 -0500

    Woah! wrong DQL string

--- src/FuentesWorks/NickelTrackerBundle/Controller/TransactionController.php
+++ src/FuentesWorks/NickelTrackerBundle/Controller/TransactionController.php
@@ -96,7 +96,7 @@ class TransactionController extends NickelTrackerController
         if($accountId) {
             $qbTransactions->andWhere('t.accountId = :accountId')
                 ->setParameter('accountId', $accountId);
-            $qbTransfers->andWhere('t.sourceId = :accountId OR t.destinationId')
+            $qbTransfers->andWhere('t.sourceId = :accountId OR t.destinationId = :accountId')
                 ->setParameter('accountId', $accountId);
         }
 
