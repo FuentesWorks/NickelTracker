0358759dd9af3b5fd5d914deba2bfba352320fa7
Edmundo Fuentes <me@edmundofuentes.com>
2014-10-15 19:34:43 -0500

    Fixing things..

--- src/FuentesWorks/NickelTrackerBundle/Controller/ReportController.php
+++ src/FuentesWorks/NickelTrackerBundle/Controller/ReportController.php
@@ -6,9 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-use FuentesWorks\NickelTrackerBundle\Entity\TransactionInterface;
-use FuentesWorks\NickelTrackerBundle\Entity\TransactionLog;
-use FuentesWorks\NickelTrackerBundle\Entity\TransferLog;
+use FuentesWorks\NickelTrackerBundle\Entity\Transaction;
 use FuentesWorks\NickelTrackerBundle\Entity\Account;
 use FuentesWorks\NickelTrackerBundle\Entity\Category;
 
