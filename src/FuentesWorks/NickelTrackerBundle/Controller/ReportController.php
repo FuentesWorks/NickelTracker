663056d0a16d8feccf86910f77677fbb9f1913f5
Edmundo Fuentes <me@edmundofuentes.com>
2014-11-01 18:49:47 -0600

    Creating reports!

--- src/FuentesWorks/NickelTrackerBundle/Controller/ReportController.php
+++ src/FuentesWorks/NickelTrackerBundle/Controller/ReportController.php
@@ -12,5 +12,31 @@ use FuentesWorks\NickelTrackerBundle\Entity\Category;
 
 class ReportController extends NickelTrackerController
 {
+    public function listAction()
+    {
+        return $this->render('FuentesWorksNickelTrackerBundle:Report:list.html.twig');
+    }
+
+    public function monthlyBalanceAction(Request $request)
+    {
+        $sql = <<<ENDSQL
+SELECT
+    YEAR(t.date) as `year`,
+    MONTH(t.date) as `month`,
+    SUM(CASE WHEN t.type = 'I' THEN t.amount ELSE 0 END) as `income`,
+    SUM(CASE WHEN t.type = 'E' THEN t.amount ELSE 0 END) as `expense`
+FROM Transactions as t
+GROUP BY YEAR(t.date), MONTH(t.date)
+ENDSQL;
+
+        /** @var \Doctrine\ORM\EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+        $stmt = $em->getConnection()->prepare($sql);
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+
+        return $this->render('FuentesWorksNickelTrackerBundle:Report:monthlyBalance.html.twig');
+    }
+
 
 }
